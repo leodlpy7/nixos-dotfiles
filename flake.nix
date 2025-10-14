b4266e1827fd7be546b383454114f7a183a24b70
leodlpy7 <leonard.delpy@gmail.com>
2025-10-14 14:10:28 +0200

    added sops

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{self, nixpkgs, home-manager, ...}: {
+  outputs = inputs@{self, nixpkgs, home-manager, sops-nix, ...}: {
     nixosConfigurations.amaterasu = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -19,6 +23,7 @@
 	    users.leo = import ./home/leo/default.nix;
 	  };
 	}
+	sops-nix.nixosModules.sops
       ];
     };
   };
